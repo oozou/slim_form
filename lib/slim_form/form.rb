84a67d66ef8fb34172b5a95b22b923b3fce66616
Joe Woodward <joe@oozou.com>
2018-09-18 16:59:58 +0700

    Add nested_forms
    NOTE will refactor later to make it more readable and intuitive

--- lib/slim_form/form.rb
+++ lib/slim_form/form.rb
@@ -2,9 +2,10 @@
 
 require 'active_model/model'
 require 'slim_form/attributes'
+require 'slim_form/custom_validators'
+require 'slim_form/nested_form'
 require 'slim_form/persistence'
 require 'slim_form/resources'
-require 'slim_form/custom_validators'
 
 module SlimForm
   module Form
@@ -12,6 +13,7 @@ module SlimForm
     include ActiveModel::Model
     include ActiveModel::Validations::Callbacks
     include SlimForm::Attributes
+    include SlimForm::NestedForm
     include SlimForm::Persistence
     include SlimForm::Resources
 
@@ -23,29 +25,40 @@ module SlimForm
       def initialize(params: {}, **resources)
         # if user passes params: nil
         raise NullParamsError unless params
-        form_resources = sanitize_resources(resources)
+        params_hash = hashify(params)
+        self.supplied_params = params_hash
+        form_resources = sanitize_resources(params_hash, resources)
         form_resources.each do |resource_accessor, object|
           public_send("#{resource_accessor}=", object)
         end
         self.supplied_resources = form_resources.keys.map(&:to_sym)
-        form_attributes = sanitize_params(params)
-        self.supplied_params = form_attributes.keys.map(&:to_sym)
+        form_attributes = sanitize_params(params_hash)
         super(form_attributes)
       end
 
-      private def sanitize_resources(hash)
-        hash.keep_if { |key, value| value.present? }
+      private def sanitize_resources(hash, resources)
+        sanitized_resources = resources.symbolize_keys.keep_if do |key, value|
+          value.present?
+        end
+        self.class.nested_forms.keys.each do |key|
+          value = hash.delete(key)
+          sanitized_resources[key] ||= value if value
+        end
+        sanitized_resources
       end
 
-      private def sanitize_params(params)
-        hash = if params.is_a?(ActionController::Parameters)
-                 params.to_unsafe_h
-               else
-                 params
-               end
+      private def sanitize_params(hash)
         hash.keep_if do |key, value|
           self.class.attributes.has_key?(key.to_sym) && value.present?
         end
+      end
+
+      private def hashify(params)
+        if params.is_a?(ActionController::Parameters)
+          params.to_unsafe_h
+        else
+          params
+        end.symbolize_keys
       end
     end
   end
---
+++ lib/slim_form/nested_form.rb
@@ -0,0 +1,85 @@
+# frozen_string_literal: true
+
+require 'active_support/concern'
+require 'active_support/core_ext/class/attribute'
+module SlimForm
+  module NestedForm
+    extend ActiveSupport::Concern
+
+    included do
+      class_attribute :nested_forms, instance_accessor: false, default: {}
+
+      def valid?
+        super
+        validate_nested_forms
+        errors.blank?
+      end
+
+      private def validate_nested_forms
+        self.class.nested_forms.keys.map do |attr|
+          form = public_send(attr)
+          next unless form
+          next if form.valid?
+          form.errors.details.each do |nested_attr, errors_array|
+            errors_array.each do |error_hash|
+              errors.add(:"#{attr}.#{nested_attr}", error_hash[:error])
+            end
+          end
+        end
+      end
+
+      def read_attribute_for_validation(attribute)
+        attribute, nested_attribute = attribute.to_s.split('.', 2)
+        if nested_attribute
+          public_send(
+            attribute
+          ).send(
+            :read_attribute_for_validation, nested_attribute
+          )
+        else
+          super
+        end
+      end
+    end
+
+    class_methods do
+      def nested_form(
+        resource_attr,
+        class_name: "#{resource_attr.to_s.classify}Form",
+        required: true,
+        &block
+      )
+        nested_forms[resource_attr.to_sym] =
+          if block_given?
+            Class.new do
+              include SlimForm::Form
+              define_singleton_method :name do
+                class_name
+              end
+              instance_eval(&block)
+            end
+          else
+            class_name.constantize
+          end
+        configure_nested_form_accessors(resource_attr)
+        validates(resource_attr, presence: true) if required
+      end
+
+      private def configure_nested_form_accessors(resource_attr)
+        attr_reader(resource_attr)
+        define_method("#{resource_attr}=") do |*form_or_args|
+          form_class = self.class.nested_forms[resource_attr.to_sym]
+          form =
+            if form_or_args[0].is_a?(form_class)
+              form_or_args[0]
+            elsif form_or_args[0][:params]
+              form_class.new(form_or_args[0])
+            else
+              form_class.new(params: form_or_args[0])
+            end
+          instance_variable_set("@#{resource_attr}", form)
+        end
+      end
+    end
+  end
+end
